c472b1d32143dbcbb040f960e2b8295e6a81e492
mythri523 <239y1a0523@ksrmce.ac.in>
2026-02-27 15:38:27 +0530

    Create home.tsx

---
+++ home.tsx
@@ -0,0 +1,13 @@
+const Home: React.FC = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Home Page</h1>
+      <img
+        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f"
+        width="400"
+      />
+    </div>
+  );
+};
+
+export default Home;
