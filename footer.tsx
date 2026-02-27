bb3bdfe729c8c30995d61864f933612a8ab176fb
mythri523 <239y1a0523@ksrmce.ac.in>
2026-02-27 15:36:13 +0530

    Create footer.tsx

---
+++ footer.tsx
@@ -0,0 +1,16 @@
+const Footer: React.FC = () => {
+  return (
+    <footer
+      style={{
+        background: "#111",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      © 2026 MyApp
+    </footer>
+  );
+};
+
+export default Footer;
