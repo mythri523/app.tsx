a6ff9d11015d63897f5126ed81e65e5ea1a4b7af
mythri523 <239y1a0523@ksrmce.ac.in>
2026-02-27 15:39:00 +0530

    Create services.tsx

---
+++ services.tsx
@@ -0,0 +1,14 @@
+const Services: React.FC = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Services</h1>
+      <ul>
+        <li>Web Development</li>
+        <li>App Development</li>
+        <li>UI/UX Design</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
